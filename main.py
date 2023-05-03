8d2731c6900329c477fc45473b2b13e08438aeb1
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-05-03 17:24:23 +0530

    create endpoints to interact with cache: PUT, GET, DELETE

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from typing import Union
+from pydantic import BaseModel
+from cacheout import Cache
+from fastapi import HTTPException
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+cache = Cache()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.put("/value/{key}")
+def create_value(key: str, value: str):
+    cache.set(key, value)
+    return {"key": key, "value": value}
 
+@app.get("/name/{key}")
+def get_value(key: str):
+    value = cache.get(key)
+    if not value:
+        raise HTTPException(status_code=404, detail=f"Value with key {key} not found")
+    return {"key": key, "value": value}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.delete("/value/{key}")
+def delete(key: str):
+    cache.delete(key)
+    return {'data': "name is deleted"}
+
+@app.get("/values/keys")
+def view_keys():
+    keys = list(cache.keys())
+    return {"keys": keys}
