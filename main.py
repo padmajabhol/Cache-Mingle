903c8daa8d6f5228ff56aec60585494d30f65519
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-05-05 20:05:55 +0530

    add endpoints and unit tests for bulk requests and cache configuration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from typing import Union
+from typing import Union, List , Dict
 from pydantic import BaseModel
 from cacheout import Cache
-from fastapi import HTTPException
+from fastapi import HTTPException, Query
 
 from fastapi import FastAPI
 
@@ -13,32 +13,83 @@ class Value(BaseModel):
     key: str
     value: str
     
-@app.put("/value/{key}")
+@app.put("/cache/{key}")
 def create_value(request: Value):
     key_value = request.key
     value_value = request.value
     cache.set(key_value, value_value)
     return {"key": key_value, "value": value_value}
 
-@app.get("/value/{key}")
+@app.get("/cache/{key}")
 def get_value(key: str):
     value = cache.get(key)
     if not value:
         raise HTTPException(status_code=404, detail=f"Value with key {key} not found")
     return {"key": key, "value": value}
 
-@app.get("/values/keys")
+@app.get("/caches/keys")
 def view_keys():
     keys = list(cache.keys())
     return {"keys": keys}
 
-@app.delete("/value/{key}")
+@app.delete("/cache/{key}")
 def delete(key: str):
     cache.delete(key)
     return {'data': "value is deleted"}
 
-@app.get("/value")
+@app.get("/cache")
 def get_entire_cache():
     value = cache.copy()
     return {"value": value}
+
+
+
+class BulkValue(BaseModel):
+    key: str
+    value: str
+
+class CreateBulkValue(BaseModel):
+    items: List[BulkValue]
+
+@app.put("/bulkcache")
+def set_bulk_value(request: CreateBulkValue):
+    for item in request.items:
+        key = item.key
+        value = item.value
+        cache.set(key, value )
+    return {"message": "Set successfully"}
+
+
+@app.get("/bulkcache")
+def get_cache_values(keys: List[str] = Query(None)):
+    values = cache.get_many(keys)
+    return {"values": values}
+
+
+class DeleteBulkValue(BaseModel):
+    keys: List[str]
+
+@app.delete("/bulkcache")
+def delete_bulk(keys: List[str] = Query(None)):
+    keys1 = []
+    for key in keys:
+        keys1.append(key)
+        cache.delete_many(keys1)
+    return {'data': 'values are deleted'}
+
+
+
+class Configure(BaseModel):
+    maxsize: int
+    ttl: int
+
+@app.put("/configure")
+def configure_cache(request: Configure):
+    maxsize_value = request.maxsize
+    ttl_values = request.ttl
+    cache.configure(maxsize=maxsize_value, ttl=ttl_values)
+    return {'data': f"new maxsize set to {maxsize_value} and ttl set to {ttl_values}"}
+    
+
+
 
--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+import json
 
 client = TestClient(app)
 
@@ -9,28 +10,81 @@ data = {
 }
 
 def test_create_value():
-    response = client.put("/value/1", json=data)
+    response = client.put("/cache/1", json=data)
     assert response.status_code == 200
     assert response.json() == data
 
 def test_get_value():
-    response = client.get("/value/1")
+    response = client.get("/cache/1")
     assert response.status_code == 200
     assert response.json() == data
 
 def test_view_keys():
-    response = client.get("/values/keys")
+    response = client.get("/caches/keys")
     assert response.status_code == 200
     assert response.json() == {"keys": list(data["key"])}
 
 def test_get_entire_cache():
-    response = client.get("/value")
+    response = client.get("/cache")
     assert response.status_code == 200
     assert response.json() == {'value': {data["key"]: data["value"]}}
 
 def test_delete():
-    response = client.delete("/value/1")
+    response = client.delete("/cache/1")
     assert response.status_code == 200
     assert response.json() == {'data': "value is deleted"}
+
+# data_bulk = {"key": "1",
+#              "value": "padmaja"}
+    
+# def test_set_bulk_value():
+#     response = client.put("/bulkvalue", json=data)
+#     assert response.status_code == 200
+#     assert response.json() == {'message': "Set successfully"}
+
+def test_set_bulk_value():
+    key1, value1 = "key1", "value1"
+    key2, value2 = "key2", "value2"
+    key3, value3 = "key3", "value3"
+    request_body = {
+        "items": [
+            {"key": key1, "value": value1},
+            {"key": key2, "value": value2},
+            {"key": key3, "value": value3},
+        ]
+    }
+
+    # Make a request to the set_bulk_value endpoint
+    response = client.put("/bulkcache", json=request_body)
+
+    # Assert that the response status code is 200
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Set successfully'}
+
+def test_get_cache_values():
+    key1, value1 = "key1", "value1"
+    key2, value2 = "key2", "value2"
+    key3, value3 = "key3", "value3"
+    request_body = {"keys": [key1, key2, key3]}
+    response = client.get("/bulkcache", params={"keys": ["key1", "key2", "key3"]})
+    assert response.status_code == 200
+    assert response.json() == {"values": {key1: value1, key2: value2, key3: value3}}
+
+def test_delete_bulk():
+    # key1 = "key1"
+    # key2 = "key2"
+    # key3 = "key3"
+    # request_body = {"keys": [key1, key2, key3]}
+    response = client.delete("/bulkcache", params={"keys": ["key1", "key2", "key3"]})
+    assert response.status_code == 200
+
+data1 = {
+    "maxsize": 256,
+    "ttl": 300
+}
+def test_configure_cache():
+    response = client.put("/configure", json=data1)
+    assert response.status_code == 200
+    assert response.json() == {"data": f"new maxsize set to {data1['maxsize']} and ttl set to {data1['ttl']}"}
 
 
